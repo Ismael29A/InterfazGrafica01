1064be79946b1bf6bd19cc715e3afed646fc4eb7
Ismael29A <aa20081@ues.edu.sv>
2025-02-12 14:54:53 -0600

    Version 1

--- app/src/main/java/com/example/interfazgrafica01/MainActivity.java
+++ app/src/main/java/com/example/interfazgrafica01/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.interfazgrafica01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText text1, direccion, resultado;
+    Button btnver;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        text1 = findViewById(R.id.text1);
+        direccion = findViewById(R.id.direccion);
+        resultado = findViewById(R.id.resultado);
+        btnver = findViewById(R.id.btnver);
+    }
+
+    public void guardar(View viem){
+        if (text1.getText().toString().isEmpty()){
+            Toast.makeText(getBaseContext(),"vacio",)
+        }
     }
 }
